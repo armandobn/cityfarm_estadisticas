bc51a43ea4942566da0f9de4768b68f58d445319
armandobn <72363199+armandobn@users.noreply.github.com>
2023-08-31 00:47:28 -0600

    diseño numeros negativos

--- src/components/RecursoComponent.vue
+++ src/components/RecursoComponent.vue
@@ -2,7 +2,8 @@
   <label for=""> <img  :srcset="props.src"  v-bind:style="{
         // 'background-color':'#ccc' //kebab-case
         'width': props.width+'%'
-    }" alt="" >: {{ props.cantidad }} </label><br>
+    }" alt="" >: <label :style="{ color: props.cantidad < 0 ? 'red' : '',
+              'fontWeight': 'bold'}">{{ props.cantidad }}</label>  </label><br>
 </template>
 <script setup>
 import { defineProps } from 'vue';
